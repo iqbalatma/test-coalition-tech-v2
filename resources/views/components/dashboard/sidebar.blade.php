ccff4bd404dc6258fdce9df7b3a4bc554ab62b67
iqbalatma <iqbalatma@gmail.com>
2023-01-13 23:54:57 +0700

    install bootstrap theme, jquery, and layouting

---
+++ resources/views/components/dashboard/sidebar.blade.php
@@ -0,0 +1,37 @@
+<div class="sidebar-menu">
+    <ul class="menu">
+        <li class="sidebar-title">Menu</li>
+
+        <li class="sidebar-item active ">
+            <a href="index.html" class='sidebar-link'>
+                <i class="bi bi-grid-fill"></i>
+                <span>Coalition Test</span>
+            </a>
+        </li>
+
+
+        <li class="sidebar-title">Raise Support</li>
+
+        <li class="sidebar-item  ">
+            <a href="https://zuramai.github.io/mazer/docs" class='sidebar-link'>
+                <i class="bi bi-life-preserver"></i>
+                <span>Documentation</span>
+            </a>
+        </li>
+
+        <li class="sidebar-item  ">
+            <a href="https://github.com/zuramai/mazer/blob/main/CONTRIBUTING.md" class='sidebar-link'>
+                <i class="bi bi-puzzle"></i>
+                <span>Contribute</span>
+            </a>
+        </li>
+
+        <li class="sidebar-item  ">
+            <a href="https://github.com/zuramai/mazer#donation" class='sidebar-link'>
+                <i class="bi bi-cash"></i>
+                <span>Donate</span>
+            </a>
+        </li>
+
+    </ul>
+</div>
